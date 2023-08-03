3a0c7e73853f8a31f3f23847ea5895a8bc09e328
Chirayu Nimonkar <chirayu.nimonkar@gmail.com>
2023-08-03 15:34:53 +0100

    Add define statements

--- mlx90395.txt.ino
+++ mlx90395.txt.ino
@@ -1,7 +1,10 @@
 #include "Adafruit_MLX90395.h"
 
-Adafruit_MLX90395 sensor = Adafruit_MLX90395();
+#define OPTIMAL_OSR MLX90395_OSR_8         // optimal OSR: 8 {1, 2, 4, 8}
+#define OPTIMAL_RESOLUTION MLX90395_RES_19 // optimal resolution: 19 {16, 17, 18, 19}
+#define OPTIMAL_GAIN_SELECTION 7           // optimal gainSel: 7 {0-15}
 
+Adafruit_MLX90395 sensor = Adafruit_MLX90395();
 void setup(void)
 {
   Serial.begin(115200); // ensure baud in the Serial monitor is set to the same value
@@ -24,8 +27,8 @@ void setup(void)
   Serial.print(sensor.uniqueID[1], HEX);
   Serial.println(sensor.uniqueID[2], HEX);
 
-  // OSR
-  sensor.setOSR(MLX90395_OSR_8); // optimal OSR: 8 {1, 2, 4, 8}
+  // Set and print OSR
+  sensor.setOSR(OPTIMAL_OSR);
   Serial.print("OSR set to: ");
   switch (sensor.getOSR()) {
     case MLX90395_OSR_1: Serial.println("1 x"); break;
@@ -34,8 +37,8 @@ void setup(void)
     case MLX90395_OSR_8: Serial.println("8 x"); break;
   }
   
-  // Resolution
-  sensor.setResolution(MLX90395_RES_19); // optimal resolution: 19 {16, 17, 18, 19}
+  // Set and print Resolution
+  sensor.setResolution(OPTIMAL_RESOLUTION);
   Serial.print("Resolution: ");
   switch (sensor.getResolution()) {
     case MLX90395_RES_16: Serial.println("16b"); break;
@@ -44,14 +47,12 @@ void setup(void)
     case MLX90395_RES_19: Serial.println("19b"); break;
   }
   
-  // Gain Selection
-  sensor.setGain(7); // optimal gainSel: 7 {0-15}
+  // Set and print Gain Selection
+  sensor.setGain(OPTIMAL_GAIN_SELECTION);
   Serial.print("Gain: "); Serial.println(sensor.getGain());
 }
 
 void loop(void) {
-
-
   /* Get a new sensor event, normalized to uTesla */
   sensors_event_t event; 
   sensor.getEvent(&event);
